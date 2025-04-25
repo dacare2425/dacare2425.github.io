4a75239826a34a67f8bfd7a5d523d6e969e99abb
dacare2425 <davidcalpar@gmail.com>
2025-04-25 20:46:31 +0200

    Accesibilidad modificaciones

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function mostrarMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("show");
+  var expanded = dropdown.classList.contains("show");
+  document.querySelector(".dropbtn").setAttribute("aria-expanded", expanded);
+  dropdown.setAttribute("aria-hidden", !expanded);
+}
+
+window.onclick = function(event) {
+  if (!event.target.closest('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+              document.querySelector(".dropbtn").setAttribute("aria-expanded", "false");
+              openDropdown.setAttribute("aria-hidden", "true");
+          }
       }
-    }
   }
+}
